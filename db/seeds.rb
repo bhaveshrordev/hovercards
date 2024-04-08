2b75c62e76f1545df54523a49664cc21676506ae
bhaveshrordev <salujabhavesh24@gmail.com>
2024-04-08 17:52:59 +0530

    added faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Person.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      address: Faker::Address.street_address,
+      phone: Faker::PhoneNumber.cell_phone_in_e164,
+      date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65)
+    )
+end
